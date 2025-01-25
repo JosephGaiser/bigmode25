a798cda00cc0bca41c640bb16a9385f77fa4e8e3
BuildTools <unconfigured@null.spigotmc.org>
2025-01-25 17:45:14 -0600

    custom dialouge style

--- scenes/carl.gd
+++ scenes/carl.gd
@@ -1,11 +1,14 @@
 class_name Carl
 extends  Node2D
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _ready():
-	animated_sprite_2d.play("idle_down")
-	
+    animated_sprite_2d.play("idle_down")
+
+
 func interact():
-	Dialogic.start("CarlCleaning")
-	
+    Dialogic.Styles.load_style("custom_bubble")
+    var layout: Node = Dialogic.start("CarlCleaning")
+    layout.register_character("Carl", animated_sprite_2d)
