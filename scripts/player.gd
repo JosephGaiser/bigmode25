4edd08883a0786ee27fba147f39009aa4d6a54a3
BuildTools <unconfigured@null.spigotmc.org>
2025-01-25 01:15:25 -0600

    fleshed out player controller, fixed collisiosn

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,14 +2,17 @@ class_name Player
 extends CharacterBody2D
 
 var input_dir: Vector2 = Vector2.ZERO
-var last_dir: Vector2 = Vector2.ZERO
+var last_dir: Vector2  = Vector2.ZERO
 
-var tile_size: int     = 16
-var moving: bool       = false
-
+@export var tile_size: int = 16
 @export var speed: float = .4
 
+var moving: bool   = false
+var can_move: bool = true
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_cast: RayCast2D = $RayCast2D
+@onready var interaction_area: Area2D = $InteractionArea
 
 
 func _ready():
@@ -17,30 +20,53 @@ func _ready():
 
 
 func _physics_process(delta: float) -> void:
-	input_dir = Vector2.ZERO
-	if Input.is_action_pressed("move_up") && input_dir != Vector2.UP:
-		input_dir = Vector2.UP
-		move()
-	elif Input.is_action_pressed("move_down") && input_dir != Vector2.DOWN:
-		input_dir = Vector2.DOWN
-		move()
-	elif Input.is_action_pressed("move_left") && input_dir != Vector2.LEFT:
-		input_dir = Vector2.LEFT
-		move()
-	elif Input.is_action_pressed("move_right") && input_dir != Vector2.RIGHT:
-		input_dir = Vector2.RIGHT
-		move()
+	if !moving:
+		handle_movement_input()
+
+	update_interaction_area()
+
+	if Input.is_action_just_pressed("interact"):
+		interact()
 
 	move_and_slide()
 
 
-# Grid based movement logic
-func move() -> void:
-	if !input_dir || moving:
-		return
+func update_interaction_area() -> void:
+	match last_dir:
+		Vector2.UP:
+			interaction_area.position = Vector2(0, -tile_size)
+		Vector2.DOWN:
+			interaction_area.position = Vector2(0, tile_size)
+		Vector2.LEFT:
+			interaction_area.position = Vector2(-tile_size, 0)
+		Vector2.RIGHT:
+			interaction_area.position = Vector2(tile_size, 0)
 
-	self.last_dir = input_dir
-	match input_dir:
+
+func handle_movement_input() -> void:
+	if Input.is_action_pressed("move_up"):
+		attempt_move(Vector2.UP)
+	elif Input.is_action_pressed("move_down"):
+		attempt_move(Vector2.DOWN)
+	elif Input.is_action_pressed("move_left"):
+		attempt_move(Vector2.LEFT)
+	elif Input.is_action_pressed("move_right"):
+		attempt_move(Vector2.RIGHT)
+
+
+func attempt_move(direction: Vector2) -> void:
+	ray_cast.target_position = direction * tile_size
+	ray_cast.force_raycast_update()
+
+	if !ray_cast.is_colliding():
+		move(direction)
+
+
+func move(direction: Vector2) -> void:
+	last_dir = direction
+	moving = true
+
+	match direction:
 		Vector2.UP:
 			animated_sprite_2d.play("walk_up")
 		Vector2.DOWN:
@@ -49,13 +75,10 @@ func move() -> void:
 			animated_sprite_2d.play("walk_left")
 		Vector2.RIGHT:
 			animated_sprite_2d.play("walk_right")
-		Vector2.ZERO:
-			animated_sprite_2d.play("idle_down")
 
-	moving = true
 	var tween: Tween        = create_tween()
-	var target_pos: Vector2 = self.position + input_dir * tile_size
-	tween.tween_property(self, "position", target_pos, self.speed)
+	var target_pos: Vector2 = position + direction * tile_size
+	tween.tween_property(self, "position", target_pos, speed)
 	tween.tween_callback(stop_moving)
 
 
@@ -69,4 +92,14 @@ func stop_moving() -> void:
 			animated_sprite_2d.play("idle_left")
 		Vector2.RIGHT:
 			animated_sprite_2d.play("idle_right")
+
 	moving = false
+
+
+func interact() -> void:
+	var interactables: Array[Node2D] = interaction_area.get_overlapping_bodies()
+
+	for obj in interactables:
+		if obj.has_method("interact"):
+			obj.call("interact")
+			break
